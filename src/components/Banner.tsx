b90c2c469a4f0ed1d368ddf58698b2da3a7fd593
hheeseung <ssigu69@gmail.com>
2023-03-06 23:55:39 +0900

    feat: 상단 배너 추가

---
+++ src/components/Banner.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { makeImagePath } from "../utils/utils";
+
+interface IBanner {
+  backdropPath?: string;
+  title?: string;
+  overview?: string;
+}
+
+const BannerImg = styled.div<{ bgimg: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgimg});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+  font-weight: 600;
+`;
+
+const Overview = styled.p`
+  font-size: 25px;
+  width: 50%;
+  line-height: 1.4;
+`;
+
+function Banner({ backdropPath, title, overview }: IBanner) {
+  return (
+    <BannerImg bgimg={makeImagePath(backdropPath || "")}>
+      <Title>{title}</Title>
+      <Overview>{overview}...</Overview>
+    </BannerImg>
+  );
+}
+
+export default Banner;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,40 @@
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import { getMovies, IGetMoviesResult } from "../API";
+import Banner from "../components/Banner";
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 function Home() {
-  return null;
+  const { isLoading, data } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner
+            backdropPath={data?.results[0].backdrop_path}
+            title={data?.results[0].title}
+            overview={data?.results[0].overview.slice(0, 150)}
+          />
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
